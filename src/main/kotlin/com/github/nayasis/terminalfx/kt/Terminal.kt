433c1bfdd521317cbc0fed6c57c91fa941364fe5
nayasis <nayasis@gmail.com>
2023-02-24 01:37:19 +0900

    add callback on success, fail and done.

--- src/main/kotlin/com/github/nayasis/terminalfx/kt/Terminal.kt
+++ src/main/kotlin/com/github/nayasis/terminalfx/kt/Terminal.kt
@@ -1,5 +1,6 @@
 package com.github.nayasis.terminalfx.kt
 
+import com.github.nayasis.kotlin.basica.core.extention.isNotEmpty
 import com.github.nayasis.kotlin.basica.core.io.exists
 import com.github.nayasis.kotlin.basica.core.string.toPath
 import com.github.nayasis.kotlin.basica.etc.error
@@ -9,6 +10,7 @@ import com.pty4j.PtyProcessBuilder
 import javafx.beans.property.SimpleObjectProperty
 import mu.KotlinLogging
 import tornadofx.runAsync
+import tornadofx.runLater
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
@@ -18,10 +20,16 @@ import java.util.concurrent.LinkedBlockingQueue
 
 private val logger = KotlinLogging.logger {}
 
+@Suppress("MemberVisibilityCanBePrivate")
 class Terminal(
-    terminalConfig: TerminalConfig = TerminalConfig(),
-    private val workingDirectory: String? = null,
-): TerminalView(terminalConfig) {
+    config: TerminalConfig = TerminalConfig(),
+    val command: List<String>,
+    val workingDirectory: String? = null,
+    val environments: Map<String,String> = mapOf("TERM" to "xterm"),
+    var onDone:((terminal: Terminal) -> Unit)? = null,
+    var onFail: ((terminal: Terminal, error: Throwable) -> Unit)? = null,
+    var onSuccess: ((terminal: Terminal, exitValue: Int) -> Unit)? = null,
+): TerminalView(config) {
 
     var process: Process? = null
         private set
@@ -38,9 +46,7 @@ class Terminal(
         commandQueue.put(command)
         runAsync {
             outputWriter.run {
-                val c = commandQueue.poll()
-                logger.trace { ">> send command : $c" }
-                write(c)
+                write(commandQueue.poll())
                 flush()
             }
         }
@@ -57,30 +63,44 @@ class Terminal(
 
     private fun initializeProcess() {
 
-        process = PtyProcessBuilder(terminalConfig.commandline?.toTypedArray() ?: arrayOf())
-            .setEnvironment(getEnvironment())
-            .apply {
-                if ( workingDirectory?.toPath()?.exists() == true) {
-                    setDirectory(workingDirectory)
+        try {
+            process = PtyProcessBuilder(command.toTypedArray())
+                .setEnvironment(getEnvironment())
+                .apply {
+                    if ( workingDirectory?.toPath()?.exists() == true) {
+                        setDirectory(workingDirectory)
+                    }
                 }
+                .start()
+
+            System.getProperty("file.encoding").let { charset ->
+                inputReader  = BufferedReader(InputStreamReader(process!!.inputStream, charset))
+                errorReader  = BufferedReader(InputStreamReader(process!!.errorStream, charset))
+                outputWriter = BufferedWriter(OutputStreamWriter(process!!.outputStream, charset))
             }
-            .start()
 
-        System.getProperty("file.encoding").let { charset ->
-            inputReader  = BufferedReader(InputStreamReader(process!!.inputStream, charset))
-            errorReader  = BufferedReader(InputStreamReader(process!!.errorStream, charset))
-            outputWriter = BufferedWriter(OutputStreamWriter(process!!.outputStream, charset))
+            focusCursor()
+            countDownLatch.countDown()
+            val exitValue = process!!.waitFor()
+            runLater {
+                onSuccess?.invoke(this, exitValue)
+            }
+        } catch (e: Throwable) {
+            runLater {
+                onFail?.invoke(this, e)
+            }
+        } finally {
+            runLater {
+                onDone?.invoke(this)
+            }
         }
-
-        focusCursor()
-        countDownLatch.countDown()
-        process!!.waitFor()
 
     }
 
     private fun getEnvironment(): Map<String, String> {
         return HashMap(System.getenv()).apply {
-            put("TERM", "xterm")
+            if(environments.isNotEmpty())
+                putAll(environments)
         }
     }
 
